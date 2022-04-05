32df968066cd2d564c8c0c035dfa8463af443f84
hcslearning <learning@hardcybersoft.cl>
2022-04-05 12:57:51 -0400

    Fix ListIterator

--- codigo/colecciones/src/cli/Main2.java
+++ codigo/colecciones/src/cli/Main2.java
@@ -1,4 +1,4 @@
-package cli;
+//package cli;
 
 import java.util.Arrays;
 import java.util.Iterator;
@@ -10,8 +10,7 @@ public class Main2 {
 	public static void main(String[] args) {
 		List<String> lenguajes = Arrays.asList("PHP", "Java", "JS", "Ruby", "C#");
 		
-		Iterator<String> iteradorNormal 	= lenguajes.iterator();
-		ListIterator<String> iteradorLista 	=  lenguajes.listIterator();
+		Iterator<String> iteradorNormal 	= lenguajes.iterator();		
 		
 		while( iteradorNormal.hasNext() ) {
 			String lenguaje = iteradorNormal.next();
@@ -20,14 +19,15 @@ public class Main2 {
 		
 		System.out.println("=====================================");
 		
-		while( iteradorLista.hasNext() ) {
+		ListIterator<String> iteradorLista 	=  lenguajes.listIterator();
+		while( iteradorLista.hasNext() ) {			
+			String lenguajeAnterior = "";
+			if( iteradorLista.hasPrevious() ) {
+				lenguajeAnterior = String.format("| Anterior = %s" ,iteradorLista.previous());
+				if( iteradorLista.hasNext() ) iteradorLista.next();
+			}
 			String lenguaje = iteradorLista.next();
-			System.out.printf("Lenguaje = %s", lenguaje);
-			
-			String lenguajeAnterior = iteradorLista.previous();
-			System.out.printf("| Lenguaje Anterior = %s %n", lenguajeAnterior);
-			
-			iteradorLista.next();
+			System.out.format("Lenguaje = %s %s %n", lenguaje, lenguajeAnterior);
 		}
 	}
 
